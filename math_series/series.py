100332309e8d026f214ba9637ec06e4f63bcf618
Matthew Holder <holdermatthew5@gmail.com>
2020-12-09 21:28:18 -0800

    sum_series done

--- math_series/series.py
+++ math_series/series.py
@@ -21,3 +21,13 @@ def lucas(n):
         if (i == n - 3):
             return y
         i += 1
+
+def sum_series(n, x=0, y=1):
+    i = 0
+    while i <= n - 3:
+        test = x
+        x = y
+        y = x + test
+        if (i == n - 3):
+            return y
+        i += 1
