287524ac1b579fd7234119da82ca01ad86ce977b
Matthew Holder <holdermatthew5@gmail.com>
2020-12-09 21:27:32 -0800

    lucas done

--- math_series/series.py
+++ math_series/series.py
@@ -9,3 +9,15 @@ def fibonacci(n):
         if (i == n - 3):
             return y
         i += 1
+
+def lucas(n):
+    x = 2
+    y = 1
+    i = 0
+    while i <= n - 3:
+        test = x
+        x = y
+        y = x + test
+        if (i == n - 3):
+            return y
+        i += 1
